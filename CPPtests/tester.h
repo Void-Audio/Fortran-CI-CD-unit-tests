cb8bdddd12bba226670ac817a1f65e58f374a11b
Derek Wingard <derekwingard@gmail.com>
2025-09-14 15:51:32 -0700

    OCD changes

--- CPPtests/tester.h
+++ CPPtests/tester.h
@@ -5,18 +5,18 @@
 #include <cmath>
 
 extern "C" {
+
     struct VOID_Verb_t;
 
-    // Fortran C bindings
     float void_rb_peek_index(VOID_Verb_t* v, size_t idx);
     size_t void_rb_get_write_index(VOID_Verb_t* v);
 }
 
 class BufferTester {
+    
 public:
-    BufferTester(VOID_Verb_t* verbPtr, size_t bufSize);
 
-    // Process a block of audio and validate ring buffer
+    BufferTester(VOID_Verb_t* verbPtr, size_t bufSize);
     void TestProcessBlock(const std::vector<float>& input);
 
 private:
